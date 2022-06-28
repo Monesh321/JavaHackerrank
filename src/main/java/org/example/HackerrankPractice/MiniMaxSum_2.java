6db79f1c7f17cea4f656cc8e7577b0126a66fd8e
Monesh321 <moneshali91@gmail.com>
2022-06-29 04:09:38 +0530

    Commit #4 2. Mini-Max Sum Problem: added alternate method(this passes all test cases in hackerrank)

--- src/main/java/org/example/HackerrankPractice/MiniMaxSum_2.java
+++ src/main/java/org/example/HackerrankPractice/MiniMaxSum_2.java
@@ -2,6 +2,7 @@ package org.example.HackerrankPractice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MiniMaxSum_2 {
@@ -16,14 +17,14 @@ public class MiniMaxSum_2 {
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
     public static void miniMaxSum(List<Integer> arr) {
-        // Write your code here
+        // My partially correct solution(12 out of 15 test cases passed)
         int nInts = arr.size();
         List<Long> longs = new ArrayList<>(nInts);
         for (Integer integer : arr) {
             longs.add(integer.longValue());
         }
-        long min = 0;
-        long max = 0;
+        long min = Integer.MAX_VALUE;
+        long max = Integer.MIN_VALUE;
         int i = 0;
         while (i < 5) {
             long sum = 0;
@@ -34,7 +35,6 @@ public class MiniMaxSum_2 {
             }
             if (sum > max) {
                 max = sum;
-                min = max;
             }
             if (sum < min) {
                 min = sum;
@@ -44,12 +44,23 @@ public class MiniMaxSum_2 {
         System.out.printf(min + " " + max);
     }
 
+    public static void miniMaxSum_alternateMethod(List<Integer> arr) {
+        // Write your code here(internet solution complete)
+        Collections.sort(arr);
+        long sum = 0;
+        for (int num : arr)
+            sum += num;
+        System.out.print((sum - arr.get(4)) + " " + (sum - arr.get(0)));
+    }
+
     public static void main(String[] args) {
         //main method
         //List<Long> arr = new ArrayList<Long>(Arrays. asList(1L, 3L, 5L, 7L, 9L));
-        List<Integer> arr = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9));
+        //List<Integer> arr = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9));
+        List<Integer> arr = new ArrayList<>(Arrays.asList(942381765, 627450398, 954173620, 583762094, 236817490));
+        //List<Integer> arr = new ArrayList<>(Arrays.asList(254961783, 604179258, 462517083, 967304281, 860273491));
 
-        MiniMaxSum_2.miniMaxSum(arr);
+        MiniMaxSum_2.miniMaxSum_alternateMethod(arr);
     }
 
 
